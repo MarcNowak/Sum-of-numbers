a0b24c35bb51e040381a917939fe1dab7f4eb4df
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-10-03 10:23:28 +0200

    Add 2. def + loop method

--- sum_of_numbers.py
+++ sum_of_numbers.py
@@ -1,8 +1,21 @@
 """ 1. range + loop """
 
+"""
 sum = 0
 for number in range(1,6):
     sum = sum + number
 
 
 print(sum)
+"""
+
+""" 2. def """
+
+def sum_of_numbers(number):
+    sum = 0
+    for number in range(1, number + 1):
+        sum = sum + number
+    return sum
+
+
+print(sum_of_numbers(28))
