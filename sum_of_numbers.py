f288c73b32d0415fdc194d7045d764833c427494
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-10-03 10:42:51 +0200

    Add 4. Arithmetic progression

--- sum_of_numbers.py
+++ sum_of_numbers.py
@@ -24,7 +24,17 @@ print(sum_of_numbers(28))
 
 """ 3. List Comprehension """
 
+"""
 def sum_of_numbers(number):
     return sum(number for number in range(1, number + 1))
+
+print(sum_of_numbers(25))
+"""
+
+""" 4. Arithmetic progression """
+
+
+def sum_of_numbers(number):
+    return (1 + number) / 2 * number
 
 print(sum_of_numbers(25))
